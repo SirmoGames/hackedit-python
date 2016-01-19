29246b796b1bae9350f83e6e4604a23dd48f7a3c
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2016-01-19 12:08:59 +0100

    Follow latest refactorings from core repo

--- hackedit_python/templates.py
+++ hackedit_python/templates.py
@@ -1,11 +1,12 @@
 """
 This module contains the COBOL templates provider plugin.
 """
+from hackedit import api
 
 
-class PyTemplatesProvider:
+class PyTemplatesProvider(api.plugins.TemplateProviderPlugin):
     def get_label(self):
         return 'Python'
 
-    def get_remote_url(self):
+    def get_url(self):
         return 'https://github.com/HackEdit/python_templates.git'
